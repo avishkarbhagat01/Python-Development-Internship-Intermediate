05ea9eb41d74439d4f72516774b1ce94ca576db3
avishkarbhagat01 <avishkarbhagat02@gmail.com>
2026-08-04 19:23:24 +0530

    Added hidden word display for Hangman game

--- Task_02_Hangman/hangman.py
+++ Task_02_Hangman/hangman.py
@@ -1,12 +1,21 @@
 import random
 from words import words
 
-print("🎮 Welcome to Hangman!")
+print("=" * 50)
+print("🎮 Welcome to Hangman Game")
+print("=" * 50)
 
 # Select a random word
 chosen_word = random.choice(words)
 
-print("\nRandom word selected successfully!")
+# For learning only (Remove this later)
+print("\nSelected Word:", chosen_word)
 
-# For learning purposes only
-print("Selected Word:", chosen_word)
+# Create hidden display
+display = []
+
+for letter in chosen_word:
+    display.append("_")
+
+print("\nHidden Word:")
+print(" ".join(display))
