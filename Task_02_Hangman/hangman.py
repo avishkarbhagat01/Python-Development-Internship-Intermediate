bc9a05749a17626742ff9a87255e578d729dc654
avishkarbhagat01 <avishkarbhagat02@gmail.com>
2026-08-04 19:34:21 +0530

    Implemented letter guessing in Hangman game

--- Task_02_Hangman/hangman.py
+++ Task_02_Hangman/hangman.py
@@ -5,11 +5,8 @@ print("=" * 50)
 print("🎮 Welcome to Hangman Game")
 print("=" * 50)
 
-# Select a random word
+# Select random word
 chosen_word = random.choice(words)
-
-# For learning only (Remove this later)
-print("\nSelected Word:", chosen_word)
 
 # Create hidden display
 display = []
@@ -18,4 +15,20 @@ for letter in chosen_word:
     display.append("_")
 
 print("\nHidden Word:")
+print(" ".join(display))
+
+# --------------------------
+# Player Guess
+# --------------------------
+
+guess = input("\nGuess a letter: ").lower()
+
+# Check if guessed letter exists
+for position in range(len(chosen_word)):
+    letter = chosen_word[position]
+
+    if letter == guess:
+        display[position] = guess
+
+print("\nUpdated Word:")
 print(" ".join(display))
